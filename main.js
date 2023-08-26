b41fbddfcfb2b84420f2fd68043ffb99f6d0e2e1
Julien <julien.denis3@gmail.com>
2023-08-26 12:56:35 -0400

    testing

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+/* for sticky banner */
+window.onscroll = function () { stickyFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
