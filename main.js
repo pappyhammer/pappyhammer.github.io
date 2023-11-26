2433c602d54caa7a87b37837f23bed0079ccb085
Julien <julien.denis3@gmail.com>
2023-11-26 15:29:36 -0400

    Rosie + about

--- main.js
+++ main.js
@@ -13,3 +13,15 @@ function stickyFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+/* https://stackoverflow.com/questions/23601792/get-only-light-colors-randomly-using-javascript */
+function getRandomColor() {
+    color = "hsla(" + Math.random() * 360 + ", 100%, 70%, 0.6)";
+    return color;
+}
+
+var getElm = document.getElementsByClassName('topic_tag');
+for(var i = 0, l = getElm.length; i < l; i++) {
+  var randomColor = getRandomColor();
+  getElm[i].style.backgroundColor =  randomColor;
+}
